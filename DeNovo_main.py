badf63f7dd7ecd9ec6d2debfd0a36ee2d036a407
miaruppel <106988503+miaruppel@users.noreply.github.com>
2022-06-13 16:22:07 -0500

    Beginner code to check how many fragments are missing

--- DeNovo_main.py
+++ DeNovo_main.py
@@ -55,7 +55,7 @@ os.chdir(r'C:\Users\miar\Desktop\data')
 #MzMLFile().store("filtered_MS2.mzML", exp)
 
 
-# In[3]:
+# In[6]:
 
 
 # parse function
@@ -67,7 +67,7 @@ def parseScanLine(input):
     return [scan_number, precursor_mz, trimmed_fragment_mz]
 
 
-# In[6]:
+# In[7]:
 
 
 # checking lines of log file and creating dictionary of scan numbers and fragment mzs
@@ -97,7 +97,7 @@ except FileNotFoundError:
     print("The file does not exist!")
 
 
-# In[9]:
+# In[8]:
 
 
 # load in MS2 scans
@@ -105,7 +105,7 @@ exp1 = MSExperiment()
 MzMLFile().load("filtered_MS2.mzML", exp1)
 
 
-# In[10]:
+# In[9]:
 
 
 # read in peptide sequence from tsv
@@ -119,32 +119,23 @@ scan2PeptideCharge = dict([(i, [x,y]) for i, x,y, in zip(tsv['Scan Number'], tsv
 scan2PeptideCharge_modified = {k:v for k,v in scan2PeptideCharge.items() if str(v[0]) != 'nan'}
 
 
-# In[18]:
+# In[10]:
 
 
 def findFragments(peptide_object, charge):
     # loop through each prefix and suffix (b and y ions, respectively)
-    try:
-        # y and b ions
-        y_ions = []
-        b_ions = []
-        for i in range(1, (len(trimmed_sequence) - 1)): # start at index of 1, end at peptide length - 1
-            y_ions.append(peptide_object.getSuffix(i))
-            b_ions.append(peptide_object.getPrefix(i))
-
-    except RuntimeError: # range above may be too large for indexing when considering modifications (ex. [15.9949])
-        print('Modifications resulted in abnormal indexing for sequence: ' + str(peptide_object))
-
-        # will have to remove "fragment" that is now the entire peptide length instead of peptide length - 1
-        y_ions.pop()
-        b_ions.pop()
+    # y and b ions
+    y_ions = []
+    b_ions = []
+    for i in range(1, (peptide_object.size())): # start at index of 1, end at peptide length - 1
+        y_ions.append(peptide_object.getSuffix(i))
+        b_ions.append(peptide_object.getPrefix(i))
         
     def loopChargeStates():
-    
         # computing fragment mzs
         # compute all y_ion mzs
         y_ion_mzs = []
-        for i in y_ions:
+        for i in y_ions[::-1]:
             for x in range(1, charge):
                 mz_y = i.getMonoWeight(Residue.ResidueType.YIon, x) / x
                 y_ion_mzs.append(mz_y)   
@@ -156,8 +147,8 @@ def findFragments(peptide_object, charge):
                 b_ion_mzs.append(mz_b)
         return y_ion_mzs, b_ion_mzs
     
+    # call nested function
     y_ion_mzs, b_ion_mzs = loopChargeStates()
-
 
     y_indices = []
     for i in y_ion_mzs:
@@ -169,13 +160,14 @@ def findFragments(peptide_object, charge):
     return y_indices, b_indices
 
 
-# In[24]:
+# In[11]:
 
 
+y_indices = []
+b_indices = []
 for s in exp1:
-    get_scan = s.getNativeID()
-    c_type, c_number, s_number = get_scan.split(' ')
-    words, scan_number = s_number.split('=')
+    s_number = s.getNativeID().split(' ')[-1]
+    _, scan_number = s_number.split('=')
     
     if scan_number in scan2frag and int(scan_number) in scan2PeptideCharge_modified:
         # isolate peptide sequence from dict
@@ -189,175 +181,216 @@ for s in exp1:
         peptide_object = AASequence.fromString(trimmed_sequence)
         
         # call findFragments function
-        y_indices, b_indices = findFragments(peptide_object, charge)
+        y, b = findFragments(peptide_object, charge)
+        y_indices.append(y)
+        b_indices.append(b)
     
     else: 
         pass 
-        
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[14]:
-
-
-# read in peptide sequence from tsv
-import pandas as pd
-tsv = pd.read_csv('HEK293T_De_Novo_053122_Glu-C_B_correctRTSenzyme_BP_realtimesearch1.tsv', sep='\t')
-
-# create dictionary with scan # as key and sequence/charge as values
-dict2 = dict([(i, [x,y]) for i, x,y, in zip(tsv['Scan Number'], tsv['Peptide'], tsv['Charge State'])])
-
-# removing all NaN sequences (not useful)
-dict2_modified = {k:v for k,v in dict2.items() if str(v[0]) != 'nan'}
-
-
-# In[15]:
-
-
-# isolate sequences/scan numbers from dict2 for scan numbers found in both dict2 and filtered spectra
-seqs = []
-scan_nums = []
-for i in spec_scans:
-    if i in dict2_modified:
-        seqs.append(dict2_modified[i][0])
-        scan_nums.append(i)
-        
-# isolate portion of sequence between the periods       
-trimmed_seqs = [] 
-for i in seqs: 
-    trimmed_seqs.append(i[2:-2]) # remove first two and last two characters in str 
 
 
 # In[12]:
 
 
-# create a peptide object for ID'd scans 
-pept_objects = []
-for i in trimmed_seqs:
-    pept_objects.append(AASequence.fromString(i))
+spectrum_list = []
+scans_list = []
+for s in exp1:
+    s_number = s.getNativeID().split(' ')[-1]
+    _, scan_number = s_number.split('=')
+    
+    if scan_number in scan2frag and int(scan_number) in scan2PeptideCharge_modified:
+        spectrum_list.append(s)
+        scans_list.append(scan_number)
+        
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+# the following code is checking for how many fragments are missing from just the first peptide sequence 
 
 
 # In[16]:
 
 
-def findFragments(input):
-    # loop through each prefix and suffix (b and y ions, respectively)
-    try:
-        # y and b ions
-        y_ions = []
-        b_ions = []
-        for i in range(1, (len(trimmed_seqs[input]) - 1)): # start at index of 1, end at peptide length - 1
-            y_ions.append(pept_objects[input].getSuffix(i))
-            b_ions.append(pept_objects[input].getPrefix(i))
+sequence = scan2PeptideCharge_modified[1707][0]     
+trimmed_sequence = sequence[2:-2] # remove first two and last two characters       
 
-    except RuntimeError: # range above may be too large for indexing when considering modifications (ex. [15.9949])
-        print('Modifications resulted in abnormal indexing for sequence: ' + str(pept_objects[input])) 
 
-        # will have to remove "fragment" that is now the entire peptide length instead of peptide length - 1
-        del y_ions[-1]
-        del b_ions[-1]
+# In[17]:
+
+
+trimmed_sequence
+
+
+# In[18]:
+
+
+peptide_object = AASequence.fromString(trimmed_sequence)
+
+
+# In[19]:
+
+
+peptide_object.size()
+
+
+# In[20]:
+
+
+y_ions = []
+b_ions = []
+for i in range(1, (peptide_object.size())): # start at index of 1, end at peptide length - 1
+    y_ions.append(peptide_object.getSuffix(i))
+    b_ions.append(peptide_object.getPrefix(i))
         
-    def loopChargeStates():
-        # isolate charges from dict2
-        charges = []
-        for i in scan_nums:
-            charges.append(dict2_modified[i][1])
-    
-        # computing fragment mzs
-        # compute all y_ion mzs
-        y_ion_mzs = []
-        for i in y_ions:
-            for x in range(1, charges[input]):
-                mz_y = i.getMonoWeight(Residue.ResidueType.YIon, x) / x
-                y_ion_mzs.append(mz_y)   
-        # compute all b_ion mzs
-        b_ion_mzs = []
-        for i in b_ions:
-            for x in range(1, charges[input]):
-                mz_b = i.getMonoWeight(Residue.ResidueType.BIon, x) / x
-                b_ion_mzs.append(mz_b)
-        return y_ion_mzs, b_ion_mzs
-    
-    y_ion_mzs, b_ion_mzs = loopChargeStates()
-    
-    # find fragments in associated spectrum
-    # dict for ALL spec scan numbers and associated spectrum
-    dict3 = dict(zip(spec_scans, all_spectra))
-    
-    # scans of interest are all in scan_nums
-    dict3_modified = {k:v for k, v in dict3.items() if k in scan_nums}
-    spectrum_list = list(dict3_modified.values()) # the spectra we care about
-
-    y_indices = []
-    for i in y_ion_mzs:
-        y_indices.append(spectrum_list[input].findNearest(i, 0.4))
-    b_indices = []
-    for i in b_ion_mzs:
-        b_indices.append(spectrum_list[input].findNearest(i, 0.4))
-
-    return y_indices, b_indices
 
 
-# In[14]:
+# In[21]:
 
 
-# 1st sequence
-y_indices, b_indices = findFragments(0)
+# b1
+print(b_ions[0])
 
 
-# In[15]:
+# In[22]:
 
 
-# 2nd sequence
-y_indices, b_indices = findFragments(1)
+# y16
+print(y_ions[::-1][0]) # reverse the list indexing 
 
 
-# In[ ]:
+# In[23]:
 
 
-# etc etc
-
-
-# In[ ]:
-
-
-# next....
-# how many times we didn't see any peaks that correspond to the fragmentation at a specific peptide bond 
+charge = scan2PeptideCharge_modified[1707][1]
+print(charge)
 
 
 # In[ ]:
 
 
 
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[24]:
+
+
+# finding bs
+def testingBs(fragment_index):
+    global mz1, indb
+    mz1 = []
+    for charge_state in range(1, charge):
+        mz1.append(b_ions[fragment_index].getMonoWeight(Residue.ResidueType.BIon, charge_state) / charge_state)
+        
+test1 = []
+for i in range(len(b_ions)):
+    testingBs(i)
+    test1.append(mz1)
+    
+indb = []
+for z in range(len(test1)):
+    for i in test1[z]:
+        indb.append(spectrum_list[0].findNearest(i, 0.4))
+
+# call the function
+testingBs(0)
+
+
+# In[29]:
+
+
+# finding ys
+def testingYs(fragment_index):
+    global mz2, indy
+    mz2 = []
+    for charge_state in range(1, charge):
+        mz2.append(y_ions[fragment_index].getMonoWeight(Residue.ResidueType.YIon, charge_state) / charge_state)
+        
+test2 = []
+for i in range(len(y_ions)):
+    testingYs(i)
+    test2.append(mz2)
+
+indy = []
+for z in range(len(test2)):
+    for i in test2[z]:
+        indy.append(spectrum_list[0].findNearest(i, 0.4))
+
+# call the function
+testingYs(0)
+
+
+# In[39]:
+
+
+# skim list for each corresponding y and b fragments based on charge states 
+def skimList(fragment_index, charge):
+    checkb = indb[fragment_index:fragment_index+charge-1]
+    checky = indy[fragment_index:fragment_index+charge-1]
+    return checkb, checky
+
+# check after every two mz (two possible charge states), so index as such: 0,2,4,6,etc
+checkb, checky = skimList(0, 3)
+
+if all(item == checkb[0] for item in checkb and checky):
+    print('All elements have a value of -1...we cannot uncover this fragment')
+else: 
+    print('At least one ion (b or y) has at least one charge state that was found in the MS2')
+
+
+# In[38]:
+
+
+# for every fragment 
+for x in range(0, len(indy), 2):
+    checkb, checky = skimList(x, charge)
+    if all(item == checkb[0] for item in checkb and checky):
+        print('All elements have a value of -1...we cannot uncover this fragment')
+    else: 
+        print('At least one ion (b or y) has at least one charge state that was found in the MS2')
 
 
 # In[ ]:
