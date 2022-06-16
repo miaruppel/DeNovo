5b0e72a96f7ca9aca439d7da8ea679dd28520b79
miaruppel <106988503+miaruppel@users.noreply.github.com>
2022-06-16 09:54:36 -0500

    More user-friendly table

--- fragmentStatisitcs.R
+++ fragmentStatisitcs.R
@@ -4,7 +4,6 @@ library(tidyverse)
 setwd("C:\\Users\\miar\\Desktop\\data")
 
 # Read in data and store as object
-# FYI, the table exported from python is quite whack (should fix)
-fragmentTable <- read_delim("fragmentsInfoTable.tsv", delim = '|')
+fragmentTable <- read_delim("fragmentInfoTable.tsv")
 
 view(fragmentTable)
