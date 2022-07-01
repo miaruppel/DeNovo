c9618f90c0b2a4035689a5b00c9ef9c7837a1235
miaruppel <106988503+miaruppel@users.noreply.github.com>
2022-07-01 16:02:46 -0500

    Implementing R script into notebook
    Ran into pathing problems (local vs anaconda) and then script output is not the same when run in notebook (more nan values, but only for sequences with no fragments missing) :')

--- fragmentStatistics.R
+++ fragmentStatistics.R
@@ -4,11 +4,11 @@ library(tidyverse)
 setwd("C:\\Users\\miar\\Desktop\\data")
 
 # Read in data and store as object
-fragmentTable <- read_delim("fragmentInfoTable.tsv", delim=" ", col_types = "icicic")
+fragmentTable <- read_delim("fragmentInfoTable.tsv", delim=" ", col_types = "iciicic")
 #view(fragmentTable)
 
 # Create histogram for distribution of missing fragments per peptide
-ggplot(data = fragmentTable, aes(x = Number_Missing)) + geom_histogram(binwidth = 1, fill='blue')
+#ggplot(data = fragmentTable, aes(x = Number_Missing)) + geom_histogram(binwidth = 1, fill='blue')
 
 x <- fragmentTable %>% 
   separate_rows(Missing_Fragment_Locations, sep=",", convert=T) %>%
@@ -17,10 +17,10 @@ x <- fragmentTable %>%
 
 
 # Create histogram for distribution of missing fragments on c-terminus side
-ggplot(data = x, aes(x = cTerminusEnd)) + geom_histogram(binwidth = 1, fill='red')
+#ggplot(data = x, aes(x = cTerminusEnd)) + geom_histogram(binwidth = 1, fill='red')
 
 # Create histogram for distribution of missing fragments on n-terminus side
-ggplot(data = x, aes(x = nTerminusEnd)) + geom_histogram(binwidth = 1, fill='orange')
+#ggplot(data = x, aes(x = nTerminusEnd)) + geom_histogram(binwidth = 1, fill='orange')
 
 # Save altered table 
 write.table(x, file='fragmentInfoTable_altered.tsv', sep='/t', row.names = FALSE)
